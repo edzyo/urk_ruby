f7b14fc280925dc502ff61fdf3037a41f7adc140
edzyo <makogin1995@gmail.com>
2020-06-08 18:26:17 +0300

    rewrite method and add test

--- lib/ukr_ruby.rb
+++ lib/ukr_ruby.rb
@@ -2,8 +2,8 @@ require "ukr_ruby/version"
 
 module UkrRuby
   class Error < StandardError; end
-  def self.raziv
-    print 'hello'
+  def self.raziv(num)
+    num.times { yield }
   end
 end
 
--- spec/ukr_ruby_spec.rb
+++ spec/ukr_ruby_spec.rb
@@ -2,4 +2,12 @@ RSpec.describe UkrRuby do
   it "has a version number" do
     expect(UkrRuby::VERSION).not_to be nil
   end
+
+  it 'return correct value' do
+    val = 0
+    UkrRuby.raziv(5){ val+= 5 }
+    expect(val).to eq(25)
+  end
+
+
 end
